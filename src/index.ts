50ae033572589dd4abdced2cd29b29ea3da697ea
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-12-01 23:21:51 +0100

    feat: add logging for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import pool from "./db";
 import logging from "./middleware/logging";
+import logger from "./logger";
 
 const app = express();
 const PORT = 3000;
@@ -15,12 +16,13 @@ app.get("/db-test", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
     res.json({ message: "Database connection successful", time: result.rows[0].now });
+    logger.info("Database connection succesful")
   } catch (error) {
-    console.error("Database connection error:", error);
+    logger.error("Database connection error:", error)
     res.status(500).json({ error: "Database connection failed" });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`)
 });
