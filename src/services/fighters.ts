e3604abdbf772c1ad84524fb659304201f7341e6
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-12-03 18:58:17 +0100

    feat: add CRUD logic to fighter service

--- src/services/fighters.ts
+++ src/services/fighters.ts
@@ -6,19 +6,22 @@ export const getFighters = async (): Promise<Fighter[]> => {
   const result = await pool.query("SELECT * FROM fighter");
   return result.rows.map(serializeFighter)
 }
-
-export const getFighterById = async (): Promise<Fighter | null> => {
-  return null
+export const getFighterById = async (id: number): Promise<Fighter | null> => {
+  const result = await pool.query("SELECT * FROM fighter WHERE id = $1", [id]);
+  return result.rows.length ? serializeFighter(result.rows[0]) : null
 }
 
-export const createFighterById = async (): Promise<Fighter | null> => {
-  return null
+export const createFighterById = async (data: Fighter): Promise<Fighter | null> => {
+  const result = await pool.query("INSERT INTO fighter (name, nickname, height, weight, reach, stance, dob) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *", [data.name, data.nickname, data.height, data.weight, data.reach, data.stance, data.dob])
+  return result.rows.length ? serializeFighter(result.rows[0]) : null
 }
 
-export const updateFighterById = async (): Promise<Fighter | null> => {
-  return null
+export const updateFighterById = async (id: number, data: Fighter): Promise<Fighter | null> => {
+  const result = await pool.query("UPDATE fighter SET name = $2, nickname = $3, height = $4, weight = $5, reach = $6, stance = $7, dob = $8 WHERE id = $1 RETURNING *",[id, data.name, data.nickname, data.height, data.weight, data.reach, data.stance, data.dob]);
+  return result.rows.length ? serializeFighter(result.rows[0]) : null
 }
 
-export const deleteFighterById = async (): Promise<Fighter | null> => {
-  return null
+export const deleteFighterById = async (id: number): Promise<Fighter | null> => {
+  const result = await pool.query("DELETE FROM fighter WHERE id = $1 RETURNING *", [id])
+  return result.rows.length ? serializeFighter(result.rows[0]) : null
 }
