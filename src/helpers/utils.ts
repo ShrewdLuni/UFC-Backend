0d8d22f58f138bc062de5bd8ee0f422037652ee1
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-12-11 14:36:41 +0100

    Update utils.ts

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -2,3 +2,8 @@ export const roundTo = (num: number, places: number = 2) => {
   const factor = 10 ** places;
   return Math.round(num * factor) / factor;
 };
+
+export const isValidDate = (date: string): boolean => {
+  const parsedDate = Date.parse(date);
+  return !isNaN(parsedDate);
+};
