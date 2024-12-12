ac7c91d5a70567e550d8837cf56fc2769691cc94
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-12-12 23:34:39 +0100

    feat: add positive constraint

---
+++ src/constraints/positive.ts
@@ -0,0 +1,9 @@
+import { ValidationConstraint } from "./interfaces";
+
+export class PositiveConstraint implements ValidationConstraint {
+  validate(value: number, fieldName: string): void {
+    if (value <= 0) {
+      throw new Error(`${fieldName} must be a positive number. Received: ${value}`);
+    }
+  }
+}
