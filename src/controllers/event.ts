f1e5c02e8c95ed203c6104859a8ce6790ccf5b2e
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-12-16 14:39:30 +0100

    feat: add base events controller

---
+++ src/controllers/event.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { getEvents, createEvent, getEventById } from "../services/event";
+
+export const getEventByIdController = async(req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    const event = await getEventById(+id)
+
+    if (!event) {
+      return res.sendStatus(400);
+    }
+    
+    return res.status(200).json(event).end();
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);    
+  }
+}
+
+export const getAllEvents = async (req: express.Request, res: express.Response) => {
+  try {
+    const events = await getEvents()
+    return res.status(200).json(events)
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+}
